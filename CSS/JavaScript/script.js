783b8936fb4ed09415049bcfe86752fa9f77772a
Janet <bamgbosejanet10@gmail.com>
2025-09-19 12:04:46 +0100

    friday

--- CSS/JavaScript/script.js
+++ CSS/JavaScript/script.js
@@ -5,22 +5,37 @@ let paragraph1 = document.getElementById("aboutme")
 let paragraph2 = document.getElementById("mycontact") 
 let paragraph3 = document.getElementById("myskills") 
 
+paragraph1.style.display = "block"
+button1.classList.add("active")
+
 button1.onclick = () => {
     paragraph1.style.display = "block"
     paragraph2.style.display = "none"
     paragraph3.style.display = "none"
+
+    button1.classList.add("active")
+    button2.classList.remove("active")
+    button3.classList.remove("active")
 }
 
 button2.onclick = () => {
     paragraph1.style.display = "none"
     paragraph2.style.display = "block"
     paragraph3.style.display = "none"
+
+    button1.classList.remove("active")
+    button2.classList.add("active")
+    button3.classList.remove("active")
 }
 
 button3.onclick = () => {
     paragraph1.style.display = "none"
     paragraph2.style.display = "none"
     paragraph3.style.display = "block"
+
+    button1.classList.remove("active")
+    button2.classList.remove("active")
+    button3.classList.add("active")
 }
 
 
